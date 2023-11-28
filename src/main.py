a42091114195d219865c262661da4836a4a3b35b
perymerdeka <ferilukmansyah@gmail.com>
2023-11-28 22:35:17 +0700

    Done: Complete Main Mapping of detail

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from scraper.debug import ReiSpiderDebug
 
 def main():
     spider: ReiSpider = ReiSpider()
-    spider.get_product_detail(url="https://www.rei.com/product/216094/baffin-young-eiger-snow-boots-kids?color=CHARCOAL/BLUE")
+    spider.get_product_data(url="https://www.rei.com/product/216094/baffin-young-eiger-snow-boots-kids?color=CHARCOAL/BLUE")
 
 def debug():
     spider: ReiSpiderDebug = ReiSpiderDebug()
@@ -18,4 +18,4 @@ def debug():
         # spider.get_data_from_json()
 
 if __name__ == "__main__":
-    debug()
+    main()
--- src/scraper/rei.py
+++ src/scraper/rei.py
@@ -1,18 +1,22 @@
 import httpx
+import json
 
 
 from typing import Any
 from selectolax.parser import HTMLParser
 from rich import print
 
-
+from scraper.utils.validation import Validation
 
 class ReiSpider(object):
-    def __init__(self)-> None:
-        pass
+    def __init__(self, validation: Validation = Validation()):
+        self.validation: Validation = validation
+        self.base_url: str = "https://www.rei.com"
 
-    def get_product_detail(self, url: str):
+    def get_product_data(self, url: str):
         headers: dict[str, Any] = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
+
+        # olah response
         response = httpx.get(url=url, headers=headers)
 
         # langkah sesudah mendapatkan data
@@ -22,7 +26,51 @@ class ReiSpider(object):
 
         soup: HTMLParser = HTMLParser(response.text)
 
+        # ambil data disini
+        product = self.get_product_detail(soup=soup)
+
+        # return hasilnya
+        return product
+
+        # scraping proses / olah data
+    def get_product_detail(self, soup: HTMLParser):
         # data mentah
         scripts = soup.css_first("script#modelData")
 
-        # scraping proses / olah data
+        # teknik parsing
+        datas = self.get_data_from_json(scripts.text())
+        print(datas)
+
+    def get_data_from_json(self, obj: str):
+        data_dict: dict[str, Any] = {}
+        datas = json.loads(obj)
+
+        # proses parsing JSON
+        product = datas["pageData"]["product"]
+        product_url = self.base_url + product["canonicalUrl"]
+        product_sizes = product["sizes"]
+        product_specs = product["techSpecs"]
+        product_size_chart = product["sizeChart"]
+        product_images = product["images"]
+        product_price = product["availablePrices"]
+        product_skus = product["skus"]
+        product_feature = product["features"]
+        product_color = product["byColor"]
+
+        phone_number = datas["openGraphProperties"]["og:phone_number"]
+
+        # proses untuk tambah data
+        data_dict["title"] = datas["title"]
+        data_dict["phone_number"] = self.validation.is_valid_phone(phone_number)
+        data_dict["product_url"] = product_url
+        data_dict["product_size"] = product_sizes
+        data_dict["product_specifications"] = product_specs
+        data_dict["product_size_chart"] = product_size_chart
+        data_dict["product_image"] = product_images
+        data_dict["product_price"] = product_price
+        data_dict["product_sku"] = product_skus
+        data_dict["product_feature"] = product_feature
+        data_dict["product_color"] = product_color
+
+        return data_dict
+
