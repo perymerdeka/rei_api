10a41da6a47fd581dfbd86599817aa8010a08f20
perymerdeka <ferilukmansyah@gmail.com>
2023-11-28 22:24:40 +0700

    Ft: Reformat

--- src/scraper/debug.py
+++ src/scraper/debug.py
@@ -7,6 +7,7 @@ from typing import Any
 
 from scraper.utils.validation import Validation
 
+
 class ReiSpiderDebug(object):
     def __init__(self, validation: Validation = Validation()):
         self.validation: Validation = validation
@@ -15,19 +16,18 @@ class ReiSpiderDebug(object):
     def get_product_detail(self, soup: HTMLParser):
         # data mentah
         scripts = soup.css_first("script#modelData")
-        
+
         # teknik parsing
         datas = self.get_data_from_json(scripts.text())
         print(datas)
 
-
-    def get_data_from_json(self,  obj: str):
+    def get_data_from_json(self, obj: str):
         data_dict: dict[str, Any] = {}
         datas = json.loads(obj)
 
         # proses parsing JSON
-        product = datas['pageData']['product']
-        product_url = self.base_url + product['canonicalUrl']
+        product = datas["pageData"]["product"]
+        product_url = self.base_url + product["canonicalUrl"]
         product_sizes = product["sizes"]
         product_specs = product["techSpecs"]
         product_size_chart = product["sizeChart"]
@@ -37,11 +37,11 @@ class ReiSpiderDebug(object):
         product_feature = product["features"]
         product_color = product["byColor"]
 
-        phone_number = datas["openGraphProperties"]['og:phone_number']
+        phone_number = datas["openGraphProperties"]["og:phone_number"]
 
         # proses untuk tambah data
-        data_dict['title'] = datas['title']
-        data_dict['phone_number'] = self.validation.is_valid_phone(phone_number)
+        data_dict["title"] = datas["title"]
+        data_dict["phone_number"] = self.validation.is_valid_phone(phone_number)
         data_dict["product_url"] = product_url
         data_dict["product_size"] = product_sizes
         data_dict["product_specifications"] = product_specs
@@ -51,7 +51,5 @@ class ReiSpiderDebug(object):
         data_dict["product_sku"] = product_skus
         data_dict["product_feature"] = product_feature
         data_dict["product_color"] = product_color
-
-
 
         return data_dict
